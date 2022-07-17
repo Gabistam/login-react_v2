0078b0f2848dab2225d8c221d826a65ea5b76e67
gabistam <gabistam@gmail.com>
2022-07-17 22:13:19 +0200

    +UseToken.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,18 @@
-import React, {useState} from "react";
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Dashboard from "../Dashboard/Dashboard";
-import Preferences from "../Preferences/Preferences";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Login from "../Login/Login";
-
+import Preferences from "../Preferences/Preferences";
+import useToken from "./useToken";
 
 function App() {
-    const [token, setToken] = useState();
+  const { token, setToken } = useToken();
 
-      if (!token) {
-        return <Login setToken={setToken} />;
-      }
+  if (!token) {
+    return <Login setToken={setToken} />;
+  }
+
   return (
     <div className="wrapper">
       <h1>Application</h1>
